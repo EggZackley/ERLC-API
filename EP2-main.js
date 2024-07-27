f1e4d3ec50fa3a50843d0ed4a31e8611bf56e5bc
EggZackley <hyrageoperator@gmail.com>
2024-07-27 00:28:31 -0500

    Create EP2-main.js
    This main.js file code was based on Episode 2 of the Series, "Integrating Roblox ERLC Private Server with Discord"

---
+++ EP2-main.js
@@ -0,0 +1,32 @@
+const { Client, EmbedBuilder, GatewayIntentBits } = require("discord.js");
+const { token, api, url, guildID, channelID } = require('./config.json')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences
+    ]
+});
+
+client.once("ready", async () => {
+    const response = await fetch(url + '/server/players', {
+        method: 'GET',
+        headers: {"Server-Key": api},
+    });
+    const data = await response.json(); 
+    const embed = new EmbedBuilder();
+    embed.setDescription("Player List");
+    data.forEach(player => {
+        let callSign = player["Callsign"];
+        if(callSign == null) {
+            callSign = "N/A"
+        }
+        embed.addFields({name: `${player["Player"]} (${player["Permission"]})`, value: `${player["Team"]} (${callSign})`});
+    });
+    embed.setTimestamp();
+    client.guilds.cache.get(guildID).channels.cache.get(channelID).send({embeds: [embed]});
+})
+
+client.login(token);
