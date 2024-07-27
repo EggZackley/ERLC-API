8276b20d424feb1882705793171c3ccb460c8295
EggZackley <hyrageoperator@gmail.com>
2024-07-27 00:22:06 -0500

    Create EP1-main.js
    This main.js file code was based on Episode 1 of the Series, "Integrating Roblox ERLC Private Server with Discord"

---
+++ EP1-main.js
@@ -0,0 +1,33 @@
+const { Client, EmbedBuilder, GatewayIntentBits } = require("discord.js");
+const { token, api, url, guildID, channelID } = require('./config.json')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences
+    ]
+});
+
+client.once("ready", async () => {
+    const response = await fetch(url + '/server', {
+        method: 'GET',
+        headers: {"Server-Key": api},
+    });
+    const data = await response.json();
+    const embed = new EmbedBuilder();
+    embed.setDescription("Server Status");
+    embed.addFields({name: "Name:", value: data["Name"]});
+    embed.addFields({name: "Owner ID:", value: data["OwnerId"].toString()});
+    embed.addFields({name: "Players:", value: data["CurrentPlayers"].toString()});
+    embed.addFields({name: "Max Players:", value: data["MaxPlayers"].toString()});
+    embed.addFields({name: "Join Key:", value: data["JoinKey"].toString()});
+    embed.addFields({name: "Account Verification:", value: data["AccVerifiedReq"].toString()});
+    embed.addFields({name: "Balance Teams:", value: data["TeamBalance"].toString()});
+    embed.setTimestamp();
+    embed.setColor("Orange")
+    client.guilds.cache.get(guildID).channels.cache.get(channelID).send({embeds: [embed]});
+})
+
+client.login(token);
